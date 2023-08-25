187addd05969f924fcd9032302d6f1bf4659e014
ssbaj <ssb9018@ajou.ac.kr>
2023-08-25 15:08:01 +0900

    first commit

--- R/mkgroup.R
+++ R/mkgroup.R
@@ -3,11 +3,12 @@
 # cn을 직접 지정하기: cn<-c(1000, 4000, 7000) ','\n')
 # df$edu2<-mkgroup(df$edu, cn)
 
-mkgroup<-function(target_variable, CuttingNumber) {
+mkgroup<-function(name_dataset, select_columns, CuttingNumber) {
    
-   if (base::missing(target_variable)) {
-	    cat("    df<-as.data.frame(df) ", '\n')
-		cat("    df$edu2<-mkgroup(df$edu, cn)  *NOTE: cn<-c( 4, 6 ) or cn<-quantile(df$edu, c(.25, .5, .75)) ", '\n')
+   if (base::missing(name_dataset)) {
+	 cat("    df<-mkgroup(df, 8, cn)  *NOTE: df=데이터셋, 8=그룹핑해야 하는 변수는 8번째 변수, cn=Cutting Number ", '\n')
+	    cat("                            *NOTE: cn<-c( 4, 6 ) or cn<-quantile(df$edu, c(.25, .5, .75)) ", '\n')
+		cat("    df[8]의 값이 1,2,3, ..., 7,8이고, cn<-c( 4, 6 )이면 1~3은 1로, 4,5는 2로, 6이상은 3으로 ", '\n')
 		return(cat("    df<-df%>%relocate(edu, .before=edu2)") ) }
  
   c2n <- function(x_x01){   
@@ -16,10 +17,19 @@ mkgroup<-function(target_variable, CuttingNumber) {
      tmp<-as.numeric(factor(x_x01, levels=groups))
 	 return(tmp) }
 
+  n0<-nrow(name_dataset[select_columns])
+
+  target_variable<-rep(NA, n0)
+
+  for(i in 1:n0){
+  target_variable[i]<-name_dataset[i,select_columns]
+  }
+
+
   if( class(target_variable) == 'character') {target_variable<-c2n(target_variable)}
   
   CuttingNumber <- as.vector(CuttingNumber)
-    
+  
   nx_0<-length(target_variable)
   mx_0<-length(CuttingNumber)
   
@@ -36,7 +46,15 @@ mkgroup<-function(target_variable, CuttingNumber) {
     if(is.na(groupIndexx_0[ix_0])) {groupIndexx_0[ix_0]<-(mx_0+1)}
     if(groupIndexx_0[ix_0]==-9999) {groupIndexx_0[ix_0]<-NA}
   }
-  groupIndexx_0<-as.vector(groupIndexx_0)
-  return(groupIndexx_0)
-  rm(nx_0, mx_0, ix_0, jx_0, groupIndexx_0, target_variable, CuttingNumber)
+
+  tmp_select_columns<-as.data.frame(groupIndexx_0)
+  name_dataset2<-cbind(name_dataset, tmp_select_columns)
+  ncolX0<-ncol(name_dataset2)
+  colnames(name_dataset2)[ncolX0] <- paste0( "g_", colnames(name_dataset[select_columns]) , sep='')
+  
+  name_dataset2<-as.data.frame(name_dataset2)
+
+return(name_dataset2)
 }
+
+
