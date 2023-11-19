7366794e0ccd8d1d93515673bef4387925210b1a
ssbaj <ssb9018@ajou.ac.kr>
2023-11-19 12:00:22 +0900

    first commit

--- R/opencsv.R
+++ R/opencsv.R
@@ -3,9 +3,9 @@
 opencsv <- function(name_dataset, header=FALSE, skip=0) {
 if (base::missing(name_dataset)) {
 	cat("", '\n')
-	cat("  # file에 한글이 포함된 csv파일 로딩 명령문 ", '\n')
-    	cat("  # file에 변수명이 포함되었으면 header=T / 변수명이 없으면 따로 지정할 필요 없음 ", '\n')
-       	cat("  # file에 1번~8번행이 설명문일 때, 설명문을 제거한 후 데이터를 로딩하려면 skip=8 ", '\n')
+	cat("  # if there is Korean in your file, use opencsv() ", '\n')
+    	cat("  # if there are variable names in the first line, header=T ", '\n')
+       	cat("  # if 1st~8th lines are comments and you want to skip them, skip=8 ", '\n')
 	cat("", '\n')
 	cat("  df<-opencsv( '가계금융복지2018.csv', header=F, skip=0 ) ", '\n')
 	return(cat("", '\n') )
