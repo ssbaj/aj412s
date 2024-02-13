a4efd60bf3b9ea9f2c1b7d9488dea9eed016e9ce
ssbaj <ssb9018@ajou.ac.kr>
2024-02-13 12:37:07 +0900

    first commit

--- R/mkgroup.R
+++ R/mkgroup.R
@@ -1,17 +1,21 @@
 # How To Use:
 # quantile로 지정: cn<-quantile(df$edu, c(.25, .5, .75))
 # cn을 직접 지정하기: cn<-c(1000, 4000, 7000) ','\n')
-# df$edu2<-mkgroup(df$edu, cn)
+# df$edu2<-mkgroup(df$edu, 8th column, cn)
 
-mkgroup<-function(name_dataset, select_columns, CuttingNumber) {
+mkgroup<-function(name_dataset, select_columns, CuttingNumber, sign=1) {
    
    if (base::missing(name_dataset)) {
-	 cat("    df<-mkgroup(df, 8, cn)  *NOTE: df=데이터셋, 8=그룹핑해야 하는 변수는 8번째 변수, cn=Cutting Number, 변수<cn 선택 ", '\n')
-	    cat("                            *NOTE: cn<-c( 4, 6 ) or cn<-quantile(df$edu, c(.25, .5, .75)) ", '\n')
-		cat("    df[8]의 값이 1,2,3, ..., 7,8이고, cn<-c( 4, 6 )이면 4미만 1, 6미만 2, 6이상 3 ", '\n')
-		return(cat("    df<-df%>%relocate(edu, .before=edu2)") ) }
+	    cat(" df<-mkgroup(df, 8, cn)   ", '\n')
+	    cat("    *NOTE: df=data, 8=8th variable, cn=Cutting Number, sign=1은 미만< ", '\n')
+	    cat("    *NOTE: cn<-c( 4, 6 ) or cn<-quantile(df$edu, c(.25, .5, .75)) ", '\n')
+		cat("    *NOTE: df[8]의 값이 1,2,3, ...,7,8이고, cn<-c( 4, 6 )이면 4미만=1, 6미만=2, 6이상=3 ", '\n')
+		cat(" df<-mkgroup(df, 8, cn, 2)  ", '\n')
+		cat("    *NOTE: df=data, 8=8th variable, cn=Cutting Number, sign=2는 이하<= ", '\n')
+		cat("    *NOTE: df[8]의 값이 1,2,3, ...,7,8이고, cn<-c( 4, 6 )이면 4이하=1, 6이하=2, 6초과=3 ", '\n')
+		return(cat("      ") ) }
  
-  c2n <- function(x_x01){   
+  c2n <- function(x_x01){ 
      groups = unique(x_x01)   
      groups= sort(groups)
      tmp<-as.numeric(factor(x_x01, levels=groups))
@@ -23,6 +27,7 @@ mkgroup<-function(name_dataset, select_columns, CuttingNumber) {
 
   for(i in 1:n0){
   target_variable[i]<-name_dataset[i,select_columns]
+  if(is.na(name_dataset[i,select_columns])) {cat(' ** Remove NAs ----','\n')}
   }
 
 
@@ -32,21 +37,52 @@ mkgroup<-function(name_dataset, select_columns, CuttingNumber) {
   
   nx_0<-length(target_variable)
   mx_0<-length(CuttingNumber)
-  
+    
   groupIndexx_0 <- matrix( rep(NA, nx_0), ncol=1)
-   
-  
+
+
+## <
+if(sign ==1 ) {
   for(ix_0 in 1:nx_0) {
-    for(jx_0 in 1:mx_0) {
-      if( is.na(target_variable[ix_0]) ) { groupIndexx_0[ix_0] <- -9999}
-      else if( target_variable[ix_0] < CuttingNumber[jx_0] ) {
-        groupIndexx_0[ix_0] <- jx_0
-        break}
-    }
-    if(is.na(groupIndexx_0[ix_0])) {groupIndexx_0[ix_0]<-(mx_0+1)}
-    if(groupIndexx_0[ix_0]==-9999) {groupIndexx_0[ix_0]<-NA}
+
+    for(jx_0 in 1:mx_0 ) {
+      countx<-0
+      if( target_variable[ix_0] >= CuttingNumber[mx_0]) {
+		  groupIndexx_0[ix_0] <- (mx_0+1) 
+		  countx<-countx+1
+		  break}
+      if( target_variable[ix_0] < CuttingNumber[jx_0]) {
+          groupIndexx_0[ix_0] <- jx_0
+		  countx<-countx+1
+		  break}
+	if (countx==1) {break}
+	}
+	}
+}
+
+
+   
+## <=
+if(sign != 1) {
+  for(ix_0 in 1:nx_0) {
+
+    for(jx_0 in 1:mx_0 ) {
+      countx<-0
+      if( target_variable[ix_0] > CuttingNumber[mx_0]) {
+		  groupIndexx_0[ix_0] <- (mx_0+1) 
+		  countx<-countx+1
+		  break}
+      if( target_variable[ix_0] <= CuttingNumber[jx_0]) {
+          groupIndexx_0[ix_0] <- jx_0
+		  countx<-countx+1
+		  break}
+	if (countx==1) {break}
+	}
+	}
   }
 
+
+  
   tmp_select_columns<-as.data.frame(groupIndexx_0)
   name_dataset2<-cbind(name_dataset, tmp_select_columns)
   ncolX0<-ncol(name_dataset2)
@@ -56,5 +92,4 @@ mkgroup<-function(name_dataset, select_columns, CuttingNumber) {
 
 return(name_dataset2)
 }
-
 
---
+++ R/reshaping.R
@@ -0,0 +1,47 @@
+# How To Use: df2<-reshaping(df, commoncol, repeating number) 
+
+reshaping<-function(df, commoncol, repeating){
+
+   if (base::missing(df)) {
+	    cat(" df={V1 V2 V3 V4 V5 V6 V7 V8}   ", '\n')
+		cat("    *NOTE: V1과 V2는 공통, V3~V8을 2개씩 끊어서 데이터세 만든 후 rbind ", '\n')
+	    cat("    *NOTE: V1 V2 V3 V4 : V1 V2 V5 V6 : V1 V2 V7 V8 - 3개의 데이터셋이 repeating", '\n')
+		cat("    *NOTE: df2<-reshaping(df, 공통컬럼은 1~k까지, k~n컬럼을 끊어서 m개 PART로 나눌 경우) ", '\n')
+		cat("    *NOTE: df2<-reshaping(df, k, m) ", '\n')
+		return(cat("      ") ) }
+
+
+
+vnames<-c('v1','v2','v3','v4','v5','v6','v7',
+          'v8','v9','v10','v11','v12','v13','v14','v15','v16','v17',
+          'v18','v19','v20','v21','v22','v23','v24','v25','v26','v27',
+          'v28','v29','v30','v31','v32','v33','v34','v35', 'v36', 'v37',
+		  'v38','v39','v40','v41','v42','v43','v44','v45', 'v46', 'v47',
+		  'v48', 'v49', 'v50')
+
+
+totalcol<-ncol(df)
+blockcol<-totalcol-commoncol
+repeatcol<-blockcol/repeating
+tmp_colnames<-commoncol+repeatcol
+
+for(i in 1:repeating){
+  startcol<-repeatcol*(i-1)+commoncol+1
+  endcol<-repeatcol*i+commoncol
+  tmp<-df[, c(1:commoncol,  startcol:endcol) ]
+
+
+    for(x in 1:tmp_colnames){
+      colnames(tmp)[x]<-vnames[x]
+    }
+
+    
+  if(i==1) {
+    Totaltmp<-tmp
+    next}
+  Totaltmp<-rbind(Totaltmp, tmp)
+}
+
+return(Totaltmp)
+}
+
