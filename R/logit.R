554bda4fcc0e4c649978f78d9bc6b31f6c018baf
ssbaj <ssb9018@ajou.ac.kr>
2024-02-22 20:17:04 +0900

    first commit

--- R/logit.R
+++ R/logit.R
@@ -1,5 +1,5 @@
 logit <- function(formula, data=data) {
-   cat("\033[1;31m       Original command: glm(formula, family = binomial(link='logit') , data)=df  \033[0m", '\n')
+   cat("\033[1;31m       Original command: glm(formula, family = binomial(link='logit') , data=df)  \033[0m", '\n')
    cat("\033[1;31m       If you want to add the subset option, please use Original command. \033[0m", '\n')
    tmp.command<-glm(formula, family = binomial(link='logit') , data=data)
 return(tmp.command) }
