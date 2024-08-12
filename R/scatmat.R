b88711eeedb09888d33768b9435cea1b0ea97823
ssbaj <ssb9018@ajou.ac.kr>
2024-08-12 13:31:55 +0900

    first commit

--- R/scatmat.R
+++ R/scatmat.R
@@ -1,12 +1,21 @@
-scatmat<-function(..., nclass=NULL) {
+scatmat<-function(... , nclass=NULL) {
 
 if (base::missing(...)) {
-	cat("  tmp<-Adata[, c('conv','satprice','age','edu','nfamily')] ", '\n')
-	cat("  tmp<-tmp[complete.cases(tmp), ]  # NA미싱데이터가 있으면 실행불가능", '\n')
+	cat("  # NA미싱데이터가 있으면 실행불가능 ------------------------------- ", '\n')
+	cat("  # tmp<-Adata[, c('conv','satprice','age','edu','nfamily')] ", '\n')
+	cat("  # tmp<-tmp[complete.cases(tmp), ]  # NA미싱데이터가 있으면 실행불가능", '\n')
 	return( cat("  scatmat(tmp) ", '\n') )
 }
 
-  pairs( cbind(...),
+cat("  # ---------------------------------------------------------------------------- ", '\n')
+cat("  # complete.cases명령문을 사용해 NA미싱데이터를 제거한 후, scatmat을 실행합니다 ", '\n')
+cat("  tmp<-Adata[, c('conv','satprice','age','edu','nfamily')] ", '\n')
+cat("  tmp<-tmp[complete.cases(tmp), ]  # NA미싱데이터가 있으면 실행불가능", '\n')
+cat("  scatmat(tmp) ", '\n')
+cat("  viewer의 그래프를 보면, 대각선 변수가 x축 변수, 교차되는 변수가 y축 변수 ", '\n')
+cat("  # ---------------------------------------------------------------------------- ", '\n')
+
+  pairs( cbind(...) ,
          panel=function(x,y){
            points(x,y)
            abline(lm(y~x), lty=2)
