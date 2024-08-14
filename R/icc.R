6652c9af8e2f432cae5801663c30bb3b205a5567
ssbaj <ssb9018@ajou.ac.kr>
2024-08-14 19:51:21 +0900

    first commit

--- R/icc.R
+++ R/icc.R
@@ -10,5 +10,5 @@ cat(" ", '\n')
   cat("\033[1;31m  # ICC for (1|id) -------------------\033[0m ", '\n')
   tmp<-data.frame( VarCorr(modelxxxx) )
   tmp_est <- tmp[1,4]/(tmp[1,4]+tmp[2,4])
-cat("ICC = ",tmp_est, '\n')
+cat("ICC =",tmp_est, '\n')
 }
