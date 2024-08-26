87a8d3a51f4e72ffcffda8d69638d87e5f4cf41a
ssbaj <ssb9018@ajou.ac.kr>
2024-08-26 17:44:47 +0900

    first commit

--- R/class_col.R
+++ R/class_col.R
@@ -1,6 +1,7 @@
 class_col <-function(df) {
   cat(" ", '\n')
   cat("\033[1;31m  # class of variables -------------------\033[0m ", '\n')
+  cat("\033[1;32m  # variable name(column number)  class------\033[0m ", '\n')
   mycolnames<-colnames(df)
   colnumber<-length( colnames(df))
   for(i in 1:colnumber) {
