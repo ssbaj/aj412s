e70d168107808fd54aa4fc9724eec4a868cd6811
ssbaj <ssb9018@ajou.ac.kr>
2024-11-10 19:13:23 +0900

    first commit

--- R/percent_change.R
+++ R/percent_change.R
@@ -1,9 +1,11 @@
 # 퍼센트로 바꾸는 명령문
 # percent_change() 함수 정의
-percent_change <- function(x) {
+percent_change <- function(x_trend) {
 
-if (base::missing(x)) {
+if (base::missing(x_trend)) {
     cat("  To maker % , multiply the result by 100.  ", '\n')
     return(cat("  df$pch <- percent_change(df$kospi) "))  }
   
-  c(NA, ( (x[-1]/x[-length(x)] ) -1) ) }
+  x_trend <- (x_trend - dplyr::lag(x_trend) ) /dplyr::lag(x_trend)
+  
+  return(x_trend) }
