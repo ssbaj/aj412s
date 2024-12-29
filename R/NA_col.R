98b9656603822fed7bbde00032b15505dd146e85
ssbaj <ssb9018@ajou.ac.kr>
2024-12-29 18:29:00 +0900

    first commit

--- R/NA_col.R
+++ R/NA_col.R
@@ -1,7 +1,11 @@
-NA_col <-function(dataset){
+na_col <-function(dataset){
   n<-ncol(dataset)
   for(i in 1:n){
-    if( sum(is.na(df[,c(i)])) >0 ) {cat( colnames(dataset)[i],':' ,sum(is.na(df[,c(i)])) , '\n')}
+    tmp.no<-sum(is.na(df[,c(i)]))
+    if( tmp.no >0 ) {
+	cat( colnames(dataset)[i], ' : ')
+	cat( '' , tmp.no )
+	cat("\033[1;31m *** \033[0m", '\n')}
     else( cat( colnames(dataset)[i], ': ', sum(is.na(df[,c(i)])), '\n') )
   }
 }
