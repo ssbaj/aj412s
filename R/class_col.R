1054990964a6c6e1cca98ba88271b7e0dba8e29a
ssbaj <ssb9018@ajou.ac.kr>
2025-01-25 13:20:21 +0900

    first commit

--- R/class_col.R
+++ R/class_col.R
@@ -4,8 +4,12 @@ class_col <-function(df) {
   cat("\033[1;32m  # variable name(column number)  \033[0m ", '\n')
   mycolnames<-colnames(df)
   colnumber<-length( colnames(df))
+
   for(i in 1:colnumber) {
-  cat( "  ", mycolnames[i],'(',i,') ' , tmp , sep="", '\n' )  }
+    tmp<-class(df[,i])
+    cat( "  ", mycolnames[i],'(',i,') ' , tmp , sep="", '\n' )  }
+  
   cat("\033[1;31m  # --------------------------------------\033[0m ", '\n')
   cat(" ", '\n')
+
   }
