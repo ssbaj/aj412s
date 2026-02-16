d8f1b95250aedaff284c603640b57e0eaaf0d704
ssbaj <ssb9018@ajou.ac.kr>
2026-02-17 06:52:41 +0900

    first commit

--- R/mkdlog.R
+++ R/mkdlog.R
@@ -1,107 +1,69 @@
-mkdlog <-function(dataset_name , ...) {
-
-# 함수명은 5글자 이상을 사용할 것을 추천함
-
-#-------------------------------------
-if (base::missing(dataset_name)) {
-cat(" df<-mkdlog(df, var1, var2, var3) *NOTE: 변수에 자연로그를 취함 ", '\n')
-    cat(" \033[1;34m# 변수의 값들은 1.0보다 커야 합니다. min()으로 최솟값을 체크하세요. \033[0m ", '\n')
-return(cat(" \033[1;34m# example of logs : log(0)=NA, log(0.1)=NA, log(1.0)=0, log(10)=2.30 \033[0m ") ) }
-
-
-# find_col2함수 ----------------
-find_col2<-function(dataset_name, index_id ){
-  tmp_colnames<-names(dataset_name)
-  n<-length(tmp_colnames) # DataSet의 총변수 갯수
-    for(i in 1:n){
-        if(index_id==tmp_colnames[i]) {return(as.numeric(i))}
-    }
+mkdlog <- function(dataset_name, ...) {
+  
+  # 1. 인자가 없을 경우 도움말 출력
+  if (base::missing(dataset_name)) {
+    cat(" 사용법: df2 <- mkdlog(dataset, var1, var2, var3)\n")
+    cat(" \033[1;34m# 입력된 변수들에 로그를 취한 후 1차 차분한 결과를 추가합니다.\033[0m\n")
+    cat(" \033[1;34m# 로그 변수명: log_변수명 / 로그차분 변수명: dlog_변수명\033[0m\n")
+    return(invisible(NULL))
   }
 
-##--------------------------------
-# 변수명을 읽는 함수
-var_names <- as.character(substitute(list(...)))[-1]
-no_var_names<-length(var_names)
-
-r=c()
-
-for(i in 1:no_var_names){
-  t_counter<-find_col2(dataset_name, var_names[i])
-  r=c(r, t_counter)
-}
-
-r_var_index_number <- r
-
-##---------------------------------
-
-
-## log전환 변수는 1개만 가능 ----------------------
-
-makinglogs<-function(dataset_name, select_columns) {
-  dataset_name<-as.data.frame(dataset_name)
-  tmp<-(dataset_name[select_columns])
+  # 2. 따옴표 없이 입력된 변수명들을 문자열 벡터로 추출
+  var_names <- as.character(substitute(list(...)))[-1]
   
-  colnames(tmp)<-c("log_")
-  n<-nrow(tmp)
-        
-  for(i in 1:n){
-     
-	  if (is.na(tmp$log_[i])) {tmp$log_[i]<-NA}
-      
-	  else if(tmp$log_[i]==0) {
-	  cat("  ", i,"th value is zero, No log-transformation",'\n')
-  	  tmp$log_[i]<-NA
-	  }
-      
-	  else if(tmp$log_[i]>0 & tmp$log_[i]<1) {
-	  cat("  ", i,"th value is 0<X<1 , replace it with NA",'\n')
-	  tmp$log_[i]<-NA
-	  }
-      
-	  else if(tmp$log_[i]<0 ) {
-	  cat("  ", i,"th value is less than 0, No log-transformation",'\n')
-	  tmp$log_[i]<-NA
-	  }
-	  
-	  else if(tmp$log_[i]>=1) {tmp$log_[i]<-log(tmp$log_[i])}
-	  
-	  else { print('  ** There are problems in log-transformation  **') }
-	  
-	  }
+  # 변수 입력이 없는 경우 처리
+  if (length(var_names) == 0) {
+    stop("로그 및 차분을 수행할 변수명을 입력해주세요.")
+  }
 
-  colnames(tmp) <- paste0( colnames(tmp) , colnames(dataset_name[select_columns]) , sep='')
-  return(tmp)
-}
+  # 결과물을 담을 데이터프레임 복사
+  res_df <- dataset_name
 
-## 로그전환 변수를 combin 하기------------------------------------
-ncounter_logs <- length(r_var_index_number)
-
-tmp_dataframe<-dataset_name[,r_var_index_number]
-tmplog_dataframe<-dataset_name[, c(1,2)]
-tmplog_dataframe<-mkindex(tmplog_dataframe)
-tmplog_dataframe<-tmplog_dataframe[, -c(2,3)]
-
-for(i in r_var_index_number) {
-  tmplog<-makinglogs(dataset_name, i)
-  tmplog_dataframe<-data.frame(tmplog_dataframe, tmplog)
-}
-  
-  tmplog_dataframe<-tmplog_dataframe[,-c(1)]
-  tmplog_dataframe2<- tmplog_dataframe - dplyr::lag(tmplog_dataframe)
-  
-  tmp2<-data.frame(tmplog_dataframe, tmplog_dataframe2)
-  
-  ncounterx <- ncol(tmplog_dataframe)/2
-  jcounterxx<-length(r_var_index_number)
-  
-  ## Changing variable names
-  for(i in r_var_index_number){
-    jcounterxx <- jcounterxx + 1
-    colnames(tmp2)[jcounterxx] <- paste( "dl_" , colnames(dataset_name)[i] , sep='')
+  # 3. 개별 변수에 대해 반복 작업 수행
+  for (v_name in var_names) {
+    
+    # 데이터셋에 해당 변수가 있는지 확인
+    if (!(v_name %in% names(dataset_name))) {
+      cat(paste0(" 경고: '", v_name, "' 변수가 데이터셋에 없습니다. 건너뜁니다.\n"))
+      next
     }
 
-tmp3<-data.frame(dataset_name, tmp2)
+    # 원본 데이터 추출
+    original_val <- dataset_name[[v_name]]
+    
+    # 로그 변환용 임시 벡터 생성
+    log_val <- rep(NA, length(original_val))
 
-return(tmp3)
+    # 4. 로그 변환 로직 (기존 로직 유지)
+    for (i in 1:length(original_val)) {
+      val <- original_val[i]
+      
+      if (is.na(val)) {
+        log_val[i] <- NA
+      } else if (val <= 0) {
+        # 0 이하인 경우 로그 불가
+        log_val[i] <- NA
+      } else if (val > 0 && val < 1) {
+        # 0<x<1 인 경우 기존 로직에 따라 NA 처리 (필요시 log(val)로 수정 가능)
+        log_val[i] <- NA
+      } else {
+        log_val[i] <- log(val)
+      }
+    }
 
+    # 5. 변수 생성 및 병합
+    log_col_name <- paste0("log_", v_name)
+    dlog_col_name <- paste0("dlog_", v_name)
+
+    # 로그 취한 값 추가
+    res_df[[log_col_name]] <- log_val
+    
+    # 로그 차분(Difference) 값 추가: 현재 로그값 - 이전 로그값
+    # dplyr 없이 기본 함수 diff를 사용하거나 lag를 직접 계산
+    res_df[[dlog_col_name]] <- c(NA, diff(log_val))
+    
+    cat(paste0(" 처리완료: ", v_name, " -> ", log_col_name, ", ", dlog_col_name, "\n"))
+  }
+
+  return(res_df)
 }
